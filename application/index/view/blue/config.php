1a46cdd758bbadeb14c4c6006858966a31770572
ZhaoGuangYue1986 <jeky_zhao@.com>
2020-04-06 14:59:06 +0800

    添加blue主题

---
+++ application/index/view/blue/config.php
@@ -0,0 +1,27 @@
+<?php return array (
+  'name' => '蓝色博客通用风格',
+  'author' => '奇点资源网',
+  'dirname' => 'blue',
+  'version' => '1.0',
+  
+  //频道页模板
+  'category_temp' =>  
+  array (
+		'list_article' => '文章列表页模板', 
+		'danye' => '单页模板', 
+  ),
+  
+  
+  //列表页模板
+  'list_temp' =>  
+  array (
+		'list_article' => '文章列表页模板', 
+
+  ),
+   //内容页模板
+  'show_temp' => 
+  array (
+	    'show_article' => '文章内容页模板', 
+  ) 
+  
+);?>
